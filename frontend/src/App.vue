d2fdfab67ead4e3ab534141f9b88b3ec550dc33b
Brayan <macedobarbosab@gmail.com>
2026-03-04 11:22:39 +0100

    cambiada organización del proyecto. NavBar arriba y FooTer abajo centrados

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="d-flex flex-column min-vh-100 justify-content-center w-100">
-    <div class="w-75 w-lg-80 align-self-center">   
+  <div class="d-flex flex-column min-vh-100 w-75 w-lg-80 mx-auto">
+
       <NavBar />
-      <main>
+      <main class="flex-grow-1 overflow-auto">
         <router-view />
       </main>
       <FooTer />
-    </div>
   </div>
+
 
 </template>
 
--- frontend/src/components/FooTer.vue
+++ frontend/src/components/FooTer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="navbar navbar-dark bg-primary position-sticky top-0">
     <div class="container">
-      <p class="mb-1">&copy; {{ new Date().getFullYear() }} MiSitio. Todos los derechos reservados.</p>
+      <p class="mb-1">&copy; {{ new Date().getFullYear() }} Xestión Tarefas.</p>
       <ul class="list-inline mb-0">
         <li class="list-inline-item"><router-link class="nav-link" to="/avisolegal" target="_blank">Aviso legal</router-link></li>
         <li class="list-inline-item"><router-link to="/politicadeprivacidad" class="nav-link" target="_blank">Política de privacidad</router-link></li>
