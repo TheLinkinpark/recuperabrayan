2ad64f7b5b27111b8e20d2e653ebaed0f13ef973
Brayan <macedobarbosab@gmail.com>
2026-03-04 09:57:54 +0100

    Readme hecho, footer creado

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <main>
         <router-view />
       </main>
+      <FooTer />
     </div>
   </div>
 
@@ -12,6 +13,7 @@
 
 <script setup>
   import NavBar from "./components/NavBar.vue";
+  import FooTer from "./components/FooTer.vue"
 </script>
 
 
